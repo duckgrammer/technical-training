8d674ce3a16fa91ed83165a972b58c8668360639
duckgrammer <panda3141592@gmail.com>
2024-09-30 10:08:30 +0200

    add field

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -3,3 +3,5 @@ from odoo import fields, models
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "estate property table"
+
+    name = fields.Char(required=True)
