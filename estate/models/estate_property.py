875ba5e9aa86c1cdf70e6e1c856a3eb67229e789
duckgrammer <panda3141592@gmail.com>
2024-09-30 12:50:22 +0200

    active false

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -3,7 +3,7 @@ from odoo import fields, models
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "estate property table"
-    active = fields.Boolean(default=True)
+    active = fields.Boolean(default=False)
 
     name = fields.Char(required=True)
     description = fields.Text()
