4154e1d210a76b03c8da5740e5044f659f76c7c4
duckgrammer <panda3141592@gmail.com>
2024-09-30 11:41:43 +0200

    set default values

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,5 @@
 from odoo import fields, models
+from datetime import datetime
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -6,9 +7,9 @@ class EstateProperty(models.Model):
 
     name = fields.Char(required=True)
     description = fields.Text()
-    bedrooms = fields.Integer()
+    bedrooms = fields.Integer(default=2)
     garden = fields.Boolean()
     garden_orientation = fields.Selection([("north", "North"),("east", "East"),("south", "South"),("west", "West")])
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
-    availibility_date = fields.Date(copy=False)
+    availibility_date = fields.Date(copy=False, default=datetime.today())
