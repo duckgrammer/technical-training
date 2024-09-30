ea8220783dd05374c2459b56be5d4d6aa1c3e8e2
duckgrammer <panda3141592@gmail.com>
2024-09-30 16:04:09 +0200

    add compute total area

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api,fields, models
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -23,3 +23,12 @@ class EstateProperty(models.Model):
 
     buyer = fields.Many2one("res.partner")
     salesperson = fields.Many2one("res.users", default=lambda self: self.env.user)
+
+    living_area = fields.Float()
+    garden_area = fields.Float()
+    total_area = fields.Float(compute="_compute_area")
+
+    @api.depends("garden_area", "total_area")
+    def _compute_area(self):
+        for record in self:
+            record.total_area = record.garden_area + record.living_area
