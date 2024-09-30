06e9ca66b77b5e93bcda4c530f6cfadc364b583a
duckgrammer <panda3141592@gmail.com>
2024-09-30 12:58:51 +0200

    add state to estate.property

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -12,4 +12,6 @@ class EstateProperty(models.Model):
     expected_price = fields.Float(required=True)
     selling_price = fields.Float(readonly=True, copy=False)
     availibility_date = fields.Date(copy=False, default=fields.Date.add(fields.Date.today(), months=3))
+    
     active = fields.Boolean('Active', default=True)
+    state = fields.Selection([("new", "New"),("received","Offer Received"),("accepted","Offer Accepted"),("sold","Sold"),("cancel","Cancelled")], required=True, copy=False, default="new")
