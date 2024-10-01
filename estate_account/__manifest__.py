b71babd257befea960693a017edd41800bd872f5
duckgrammer <panda3141592@gmail.com>
2024-10-01 16:35:15 +0200

    import         'views/estate_property_views.xml',

--- estate_account/__manifest__.py
+++ estate_account/__manifest__.py
@@ -4,7 +4,7 @@
     "application": True,  # This line says the module is an App, and not a module
     "depends": ["estate", "account"],  # dependencies
     "data": [
-
+        'views/estate_property_views.xml',
     ],
     "installable": True,
     'license': 'LGPL-3',
