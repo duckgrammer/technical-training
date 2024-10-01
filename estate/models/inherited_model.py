807ee7b991898e26d24753e833102e3c78c21c04
duckgrammer <panda3141592@gmail.com>
2024-10-01 11:56:40 +0200

    salesperson

--- estate/models/inherited_model.py
+++ estate/models/inherited_model.py
@@ -2,4 +2,4 @@ from odoo import fields, models
 
 class ResUsers(models.Models):
     _inherit = "res.users"
-    property_ids = fields.One2many("estate.property", domain=[('state', 'is in', ['new','recieved'])])
+    property_ids = fields.One2many("estate.property", "salesperson", domain=[('state', 'is in', ['new','recieved'])])
