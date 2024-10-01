8a45e7579a1269bf16dda5621619b1247a4a9246
duckgrammer <panda3141592@gmail.com>
2024-10-01 14:28:07 +0200

    create invoice

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,7 +1,22 @@
-from odoo import models
+from odoo import models, Command
 
 class EstateProperty(models.Model):
     _inherit = "estate.property"
 
     def sold_property(self):
+        vals_list = []
+        for record in self:
+            vals_list.append({
+                "partner_id": record.buyer,
+                "move_types": "out_invoice",
+                "line_ids": [
+                    Command.create({
+                        "name": "administrative fees",
+                        "quantity": 1,
+                        "price_unit": 100,
+                    })
+                ],
+            })
+        self.env["account.move"].create(vals_list)
+
         return super().sold_property()
