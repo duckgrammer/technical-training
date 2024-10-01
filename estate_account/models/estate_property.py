5473725724f3f7d461675f0a7fd4aa589991c672
duckgrammer <panda3141592@gmail.com>
2024-10-01 14:34:44 +0200

    parter id

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -6,8 +6,9 @@ class EstateProperty(models.Model):
     def sold_property(self):
         vals_list = []
         for record in self:
+            print(record.buyer)
             vals_list.append({
-                "partner_id": record.buyer,
+                "partner_id": record.buyer.id,
                 "move_type": "out_invoice",
                 "line_ids": [
                     Command.create({
